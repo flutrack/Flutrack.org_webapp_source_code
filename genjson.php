6e822829e7b5e90f251ac822232f498dee2aff48
ploupas <panagiotisloupas@gmail.com>
2014-12-13 14:35:22 +0200

    Update genjson.php
    use correct (last week) query

--- genjson.php
+++ genjson.php
@@ -3,8 +3,8 @@ if (file_exists("results.json")){
 	if (copy("results.json","results_old.json")) {
 		unlink("results.json");
 		require_once('db.php');
-		// $result_json = mysql_query("SELECT user_name, tweet_text, latitude, longitude, tweet_date, aggravation FROM DB_table.flu_tweets WHERE UNIX_TIMESTAMP(NOW()) - tweet_date < 864000;");
-                $result_json = mysql_query("SELECT user_name, tweet_text, latitude, longitude, tweet_date, aggravation FROM DB_table.flu_tweets WHERE tweet_date > 1362873600 AND tweet_date < 1363737600;");
+		$result_json = mysql_query("SELECT user_name, tweet_text, latitude, longitude, tweet_date, aggravation FROM DB_table.flu_tweets WHERE UNIX_TIMESTAMP(NOW()) - tweet_date < 864000;");
+                //$result_json = mysql_query("SELECT user_name, tweet_text, latitude, longitude, tweet_date, aggravation FROM DB_table.flu_tweets WHERE tweet_date > 1362873600 AND tweet_date < 1363737600;");
 		// UNIX_TIMESTAMP(NOW()) - tweet_date < 604800;
 		$rows = array();
 		while($r = mysql_fetch_assoc($result_json)) {
