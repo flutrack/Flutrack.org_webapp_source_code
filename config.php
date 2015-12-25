83fe10dd0b28bd8dc672a9d151a9ee2c415941af
ploupas <panagiotisloupas@gmail.com>
2015-12-25 16:18:22 +0200

    Create config.php
    ploupas-patch-issue7

---
+++ config.php
@@ -0,0 +1,11 @@
+// used by db.php
+
+$db_user='********';
+$db_pass='**********************';
+$db_name = '********************';
+$correct_secret_word = '*********************';
+
+// used by oauth.php
+
+$consumer_key = '***********************';
+$consumer_secret = '*********************************';
